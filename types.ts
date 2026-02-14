2286cb5e87365b20ceaf958dbbf8774c1244d1b2
sliner <sophieliner@gmail.com>
2026-02-14 13:00:09 +0100

    Add files via upload

---
+++ types.ts
@@ -0,0 +1,14 @@
+
+export interface VideoItem {
+  id: string;
+  title: string;
+  youtubeId: string;
+  description: string;
+}
+
+export interface ProjectItem {
+  id: string;
+  title: string;
+  category: string;
+  imageUrl: string;
+}
