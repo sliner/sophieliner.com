7041fa1165e4106a0841b00f58f23dffc7f0fa22
sliner <sophieliner@gmail.com>
2026-03-06 12:48:58 +0100

    feat: Update painting image URLs
    Updated image and gallery URLs for several paintings on the Paintings page. This ensures that the correct artwork is displayed and linked.

--- pages/Paintings.tsx
+++ pages/Paintings.tsx
@@ -15,30 +15,31 @@ const paintings: Painting[] = [
     id: 1,
     title: "Tung Tung Tung Sahur",
     year: "2025",
-    imageUrl: "https://i.imgur.com/zz0aIuA.jpeg",
-    galleryUrl: "https://imgur.com/gallery/tung-tung-zz0aIuA",
+    imageUrl: "https://i.imgur.com/8T0Zb46.jpg",
+    galleryUrl: "https://imgur.com/8T0Zb46",
     medium: "Oil on wood"
   },
   {
     id: 2,
     title: "Brr Brr Patapim",
     year: "2025",
-    imageUrl: "https://i.imgur.com/anaxF4g.jpeg",
+    imageUrl: "https://i.imgur.com/anaxF4g.jpg",
+    galleryUrl: "https://imgur.com/anaxF4g",
     medium: "Oil on wood"
   },
   {
     id: 3,
     title: "Boneca Ambalabu",
     year: "2025",
-    imageUrl: "https://i.imgur.com/HU6i23j.jpeg",
-    galleryUrl: "https://imgur.com/gallery/boneca-HU6i23j",
+    imageUrl: "https://i.imgur.com/v6LAmM8.jpg",
+    galleryUrl: "https://imgur.com/v6LAmM8",
     medium: "Oil on wood"
   },
   {
     id: 4,
     title: "Big white pigeon",
     year: "2024",
-    imageUrl: "https://i.imgur.com/7t5Ly2s.jpeg",
+    imageUrl: "https://i.imgur.com/7t5Ly2s.jpg",
     galleryUrl: "https://imgur.com/gallery/big-white-pigeon-7t5Ly2s",
     medium: "Oil on wood"
   },
@@ -46,7 +47,8 @@ const paintings: Painting[] = [
     id: 5,
     title: "Beanstalk's cherry",
     year: "2025",
-    imageUrl: "https://i.imgur.com/4MmdbQ4.jpeg",
+    imageUrl: "https://i.imgur.com/4MmdbQ4.jpg",
+    galleryUrl: "https://imgur.com/4MmdbQ4",
     medium: "Oil on wood"
   }
 ];
